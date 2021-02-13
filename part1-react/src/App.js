04aa99486f5a1877a034822b56ed8746a4dd4b8c
Floren Llanos <florenllanos@gmail.com>
2021-02-13 19:44:50 +0100

    First commit

---
+++ part1-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+
+function App() {
+  // Comment
+  return (
+    <Mensaje />
+  );
+}
+
+export default App;
---
+++ part1-react/src/Mensaje.js
@@ -0,0 +1,11 @@
+// My component
+const Mensaje = () => {
+    return (
+        <div>
+            <h1>Hola mundo!!!!!</h1>
+            <h1>Desde el módulo</h1>
+        </div>
+    );
+}
+
+export default Mensaje;
